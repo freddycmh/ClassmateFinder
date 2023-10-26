ba688907ca17ebe0181899abb3343af94610c1ca
Freddy Mok <fleddymok@gmail.com>
2023-10-26 10:18:34 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import Home from './components/Home'
+import Login from './components/Login';
+
+function App() {
+  return (
+    <div>
+      {/* <Login /> */}
+      <Home/>
+      
+    </div>
+  );
+}
+
+export default App;
