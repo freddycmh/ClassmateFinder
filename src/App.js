6bc150e62cb9df00fa92bd8eaf0ee80aa087471f
Freddy Mok <fleddymok@gmail.com>
2023-11-07 00:52:42 -0500

    updated layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
+import React from 'react';
+import Header from './Components/Header'
+import './App.css';
+import Signup from './Pages/Signup';
+import Footer from './Components/Footer';
+import Card from './Components/Card';
+import { useState } from 'react';
 
-import Home from './components/Home'
-import Login from './components/Login';
+
 
 function App() {
+const [tabs, useTabs] = useState([
+  { name: 'classmate finder', img:'https://static.vecteezy.com/system/resources/previews/016/062/036/non_2x/quiz-time-logo-icon-symbol-cartoon-yellow-bubble-speech-vector.jpg'},
+  { name: 'quizcard', img:'https://static.vecteezy.com/system/resources/previews/016/062/036/non_2x/quiz-time-logo-icon-symbol-cartoon-yellow-bubble-speech-vector.jpg'},
+  { name: 'class', img: 'https://static.vecteezy.com/system/resources/previews/016/062/036/non_2x/quiz-time-logo-icon-symbol-cartoon-yellow-bubble-speech-vector.jpg' },
+  
+  
+  
+
+  ]);
+
   return (
-    <div>
-      <Login />
-      {/* <Home/> */}
+    <div className="App">
+      <Header />
+
+     <br/>
+      <div className='actionCard'>
+          {tabs.map((tabs) => {
+            return (
+              < Card name={tabs.name}  img={tabs.img} />
+            )
+          })
+          }
+      </div>
+     
+
       
+      {/* <Footer className="footer"/> */}
+
     </div>
   );
 }
