897fc34ec43f9aec4ea5079f3b6ff4da1e799996
Freddy Mok <fleddymok@gmail.com>
2023-11-08 16:20:18 -0500

    fixed the routing and fixed the card

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,20 +1,31 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
-const Card = (props) => {
+const cardStyle = {
+  width: '200px', // Set the desired width for the cards
+  height: '300px', // Set the fixed height for the cards
+  border: '1px solid #ddd', // Add a border for clarity
+  display: 'inline-block', // Prevent cards from stacking vertically
+  margin: '10px', // Add some margin for spacing
+  textAlign: 'center', // Center align the content
+  overflow: 'hidden', // Hide any content that exceeds the fixed height
+};
+
+const imgStyle = {
+  width: '100%',
+  height: '80%', // Adjusted to fill 80% of the card's height
+  objectFit: 'cover', // Ensure the image covers the entire space
+};
+
+function Card({ name, img }) {
   return (
-    <div className="card" style={{ width: "18rem" }}>
-      <img src={props.img} className="card-img-top" alt="..." />
-      <div className="card-body">
-        <p className="card-text">{props.name}</p>
+    <Link to={`/${name.toLowerCase()}`}> {/* This defines the route */}
+      <div style={cardStyle} className="card">
+        <img src={img} alt={name} style={imgStyle} />
+        <h4 style={{ marginBottom: 0 }}>{name}</h4>
       </div>
-    </div>
+    </Link>
   );
 }
-
-Card.propTypes = {
-  img: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired,
-};
 
 export default Card;
