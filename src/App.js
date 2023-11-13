ac9438e10f7b5a07a43f8cdee2261ef9f5b55ead
Kyaw Nanda <kyawnanda.lucky@gmail.com>
2023-11-13 00:56:19 -0500

    feat: added pages addClass and LoginPause for login restricted features refactor: edited Navbar, login page, register page fix: test and fixed data flow

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './Pages/Home';
 import LoginPage from './Pages/LoginPage';
 import SignupPage from './Pages/SignupPage';
+import Class from './Pages/Class';
+import Student from './Pages/Student';
+import LoginPause from './Pages/LoginPause';
+import AddClass from './Pages/AddClass';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import { AuthProvider } from './Components/AuthContext';
@@ -14,18 +18,15 @@ function App() {
 
       <AuthProvider>
         <div className="App">
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-
-            </ul>
-          </nav>
+          
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<LoginPage />} />
             <Route path="/signup" element={<SignupPage />} />
+            <Route path="/class/:classname" element={<Class />} />
+            <Route path="/student/:studentData" element={<Student />} />
+            <Route path="/loginpause" element={<LoginPause />} />
+            <Route path="/addclass" element={<AddClass />} />
 
           </Routes>
           <Footer />
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -9,21 +9,16 @@ function Header() {
 
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="#">
-        Navbar
-      </a>
+      <span className="navbar-brand">
+        Class Helper
+      </span>
 
       {/* Navigation links */}
       <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav">
-          <li className="nav-item">
-            <Link to="/" className="nav-link">
+          <li className="nav-item" style={{ borderLeft: '1px solid black', borderRight: '1px solid black', transition: 'border-color 0.3s' }}>
+            <Link to="/" className="nav-link" style={{ display: 'block', padding: '10px' }}>
               Home
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/class" className="nav-link">
-              Class
             </Link>
           </li>
           {/* Add other navigation links as needed */}
@@ -35,7 +30,7 @@ function Header() {
         {user ? (
           <>
             <li className="nav-item">
-              <span className="nav-link">{user}</span>
+              <span className="nav-link">{user.name}</span>
             </li>
             <li className="nav-item" onClick={logout}>
               <button className="btn btn-danger">Logout</button>
